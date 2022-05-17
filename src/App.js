483195cda726a0e0b9a39340556b14ac4a0b557d
Fahad <daylight1498@gmail.com>
2022-05-17 18:09:03 +0530

    Need to fix map leaflet.css issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from "axios";
 import "./App.css";
+// import "leaflet/dist/leaflet.css"; Need fixing css loader issue to load map
 
 import LeftContainer from "./components/leftApp/LeftContainer";
 import RightContainer from "./components/rightApp/RightContainer";
